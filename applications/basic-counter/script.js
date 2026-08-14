afc62a7d404fecb4c37d39cdadd5015b490b24c1
Reda <mahmoudreda457@gmail.com>
2026-08-14 18:23:20 +0300

    Refactor basic counter and mapping logic to use switchMap and merge for improved control flow

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,20 +1,49 @@
-import { fromEvent, interval, merge, NEVER, scan, skipUntil, takeUntil } from 'rxjs';
+import {
+  fromEvent,
+  interval,
+  mapTo,
+  merge,
+  NEVER,
+  scan,
+  skipUntil,
+  switchMap,
+  takeUntil,
+} from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
-const start$ = fromEvent(startButton, 'click');
-const pause$ = fromEvent(pauseButton, 'click');
+// const start$ = fromEvent(startButton, 'click');
+// const pause$ = fromEvent(pauseButton, 'click');
 
 // let interval$ = interval(1000);
 // let subscription;
 
 // start$.subscribe(() => subscription = interval$.subscribe(setCount));
-
 // pause$.subscribe(() => subscription.unsubscribe());
 
-const counter$ = interval(1000).pipe(
-  skipUntil(start$),
-  scan(total => total + 1, 0),
-  takeUntil(pause$)
-)
+// --------------
 
-counter$.subscribe(setCount)
+// const counter$ = interval(1000).pipe(
+//   skipUntil(start$),
+//   scan(total => total + 1, 0),
+//   takeUntil(pause$)
+// )
+
+// counter$.subscribe(setCount)
+
+// --------------
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+
+const counter$ = merge(start$, pause$).pipe(
+  switchMap((shouldIBeRunning) => {
+    if (shouldIBeRunning) {
+      return interval(1000);
+    } else {
+      return NEVER;
+    }
+  }),
+  scan(total => total + 1, 0)
+);
+
+counter$.subscribe(setCount);
--- applications/mapping/script.js
+++ applications/mapping/script.js
@@ -1,5 +1,16 @@
 import { of, from, interval, fromEvent, merge, NEVER } from 'rxjs';
-import { pluck, concatMap, take, map } from 'rxjs/operators';
+import {
+  pluck,
+  concatMap,
+  take,
+  map,
+  mapTo,
+  startWith,
+  tap,
+  switchMap,
+  mergeMap,
+  shareReplay,
+} from 'rxjs/operators';
 
 import {
   getCharacter,
@@ -10,5 +21,17 @@ import {
 } from './utilities';
 
 const character$ = from(getCharacter(1)).pipe(pluck('name'));
-
 character$.subscribe(render);
+
+// const characters$ = interval(1000).pipe(mergeMap(getCharacter));
+const characters$ = interval(1000).pipe(mergeMap(getCharacter), shareReplay(0));
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+const isRunning$ = merge(start$, pause$).pipe(
+  startWith(false),
+  tap(setStatus),
+  switchMap((isRunning) => (isRunning ? characters$ : NEVER)),
+  tap(render),
+);
+isRunning$.subscribe();
