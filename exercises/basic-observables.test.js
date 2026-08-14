0ff533c8f62235956a3500eea4cc9d1f1e26ea70
Reda <mahmoudreda457@gmail.com>
2026-08-14 09:29:52 +0300

    of & from

--- exercises/basic-observables.test.js
+++ exercises/basic-observables.test.js
@@ -2,16 +2,22 @@ import { from, of } from 'rxjs';
 
 describe('Basic Observables', () => {
   describe(of, () => {
-    it.skip('should create an observable from its arguments', () => {
+    it('should create an observable from its arguments', () => {
+      const example$ = of(1, 2, 3, 4)
       const result = [];
+
+      example$.subscribe(value => result.push(value))
 
       expect(result).toEqual([1, 2, 3, 4]);
     });
   });
 
   describe(from, () => {
-    it.skip('should create an observable', () => {
+    it('should create an observable', () => {
+      const example$ = from([1, 2, 3, 4])
       const result = [];
+
+      example$.subscribe(val => result.push(val))
 
       expect(result).toEqual([1, 2, 3, 4]);
     });
