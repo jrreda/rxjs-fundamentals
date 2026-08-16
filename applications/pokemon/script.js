c3fa188995138e026e8f8b7c9a10f1233662a1ae
Reda <mahmoudreda457@gmail.com>
2026-08-16 20:49:56 +0300

    pokemon api

--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -15,6 +15,8 @@ import {
   take,
   EMPTY,
   pluck,
+  mergeAll,
+  exhaustMap,
 } from 'rxjs';
 
 import { fromFetch } from 'rxjs/fetch';
@@ -23,25 +25,43 @@ import {
   addResults,
   addResult,
   clearResults,
+  endpoint,
   endpointFor,
   search,
   form,
+  renderPokemon,
 } from '../pokemon/utilities';
+import { identity } from 'lodash';
 
-const endpoint = 'http://localhost:3333/api/pokemon/search/';
+// const endpoint = 'http://localhost:3333/api/pokemon/search/';
 
-const search$ = fromEvent(search, 'input').pipe(
-  debounceTime(300),
-  map((event) => event.target.value),
-  distinctUntilChanged(),
-  switchMap((searchTerm) => {
-    return fromFetch(endpoint + searchTerm + '?delay=1000&chaos=true').pipe(
-      mergeMap((res) => res.json()),
-    );
-  }),
-  tap(clearResults),
-  pluck('pokemon'),
-  tap(addResults),
+const getPokemon = (searchTerm) =>
+  fromFetch(endpoint + searchTerm + '?delay=1000&chaos=true').pipe(
+    mergeMap((res) => res.json()),
+  );
+
+const getAdditionalData = (pokemon) =>
+  fromFetch(endpointFor(pokemon.id)).pipe(mergeMap((res) => res.json()));
+
+const search$ = fromEvent(form, 'submit').pipe(
+  // debounceTime(300),
+  map(() => search.value),
+  // distinctUntilChanged(),
+  exhaustMap((seachTerm) =>
+    getPokemon(seachTerm).pipe(
+      pluck('pokemon'),
+      mergeMap(identity),
+      take(1),
+      switchMap((pokemon) => {
+        const pokemon$ = of(pokemon);
+        const additionalData$ = getAdditionalData(pokemon).pipe(
+          map((data) => ({ ...pokemon, data })),
+        );
+        return merge(pokemon$, additionalData$);
+      }),
+    ),
+  ),
+  tap(renderPokemon),
 );
 
 search$.subscribe(console.log);
